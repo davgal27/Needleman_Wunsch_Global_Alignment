41b4a1d4e5153b4c0b485fb933cb5c5a611446a2
davgal27 <davegalea27@gmail.com>
2026-02-24 17:11:57 +0100

    Finished matrix computing

--- implementation.py
+++ implementation.py
@@ -30,5 +30,36 @@ scoring_matrix[:,0] = np.arange(len(seq1) + 1) * gap_score
 traceback_matrix[0:,1:] = "H"
 traceback_matrix[1:,0] = "V"
 
+
+## DEFINING THE LOOP FOR SCORING AND TRACEBACK
+for i in range(1, len(seq1) +1):
+	for j in range(1, len(seq2) + 1):
+		# Diagonal score
+		if seq1[i-1] == seq2[j-1]:
+			diag_score = scoring_matrix[i-1,j-1] + match_score
+		else:
+			diag_score = scoring_matrix[i-1,j-1] + mismatch_score
+		# vertical score
+		vert_score = scoring_matrix[i-1, j] + gap_score
+		#horizontal
+		horiz_score = scoring_matrix[i, j-1] + gap_score
+
+		## CONDITIONS TO FILL SCORING AND TRACEBACK MATRICES
+		best_score = max([diag_score, vert_score, horiz_score])
+		direction = []
+		if diag_score == best_score:
+			direction.append("D")
+			traceback_matrix[i,j] = ''.join(direction)
+			scoring_matrix[i,j] = best_score
+		if vert_score == best_score:
+			direction.append("V")
+			traceback_matrix[i,j] = ''.join(direction)
+			scoring_matrix[i,j] = best_score
+		if horiz_score == best_score:
+			direction.append("H")
+			traceback_matrix[i,j] = ''.join(direction)
+			scoring_matrix[i,j] = best_score
+
 print(traceback_matrix)
 print(scoring_matrix)
+
